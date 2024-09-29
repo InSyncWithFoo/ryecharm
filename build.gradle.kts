54d690d18e919f44583946172cd84102dae78704
InSyncWithFoo <insyncwithfoo@gmail.com>
2024-09-29 00:49:34 +0000

    Temporarily disable `buildSearchableOptions`

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,6 +76,8 @@ dependencies {
 
 // Configure IntelliJ Platform Gradle Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
 intellijPlatform {
+    buildSearchableOptions = false
+    
     pluginConfiguration {
         name = providers.gradleProperty("pluginName")
         version = providers.gradleProperty("pluginVersion")
