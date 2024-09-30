e6bda1a1632713768719f8d2a362ebae9af5dcc8
InSyncWithFoo <insyncwithfoo@gmail.com>
2024-09-30 00:59:02 +0000

    Minor refactoring

--- src/main/kotlin/insyncwithfoo/ryecharm/PathsAndFiles.kt
+++ src/main/kotlin/insyncwithfoo/ryecharm/PathsAndFiles.kt
@@ -1,11 +1,7 @@
 package insyncwithfoo.ryecharm
 
 import com.intellij.execution.configurations.PathEnvironmentVariableUtil
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.SystemInfo
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiFile
-import com.jetbrains.python.psi.PyFile
 import java.nio.file.InvalidPathException
 import java.nio.file.Path
 import kotlin.io.path.div
@@ -74,38 +70,3 @@ internal fun Path.findExecutableChild(name: String) =
 internal fun findExecutableInPath(name: String): Path? {
     return PathEnvironmentVariableUtil.findInPath(name.toOSDependentFileName())?.toPath()
 }
-
-
-internal val VirtualFile.isRyeConfigToml: Boolean
-    get() = parent?.name == ".rye" && name == "config.toml"
-
-
-internal val VirtualFile.isPyprojectToml: Boolean
-    get() = name == "pyproject.toml"
-
-
-internal val VirtualFile.isUVLock: Boolean
-    get() = name == "uv.lock"
-
-
-internal val VirtualFile.isUVToml: Boolean
-    get() = name == "uv.toml"
-
-
-internal val VirtualFile.isRuffToml: Boolean
-    get() = name == "ruff.toml" || name == ".ruff.toml"
-
-
-// https://github.com/InSyncWithFoo/ryecharm/issues/5
-private val PsiFile.isReST: Boolean
-    get() = virtualFile?.extension == "rst"
-
-
-// TODO: .ipynb / Allow configuring what files
-internal fun VirtualFile.isSupportedByRuff(project: Project? = null): Boolean {
-    return extension == "py" || extension == "pyi"
-}
-
-
-internal val PsiFile.isSupportedByRuff: Boolean
-    get() = this is PyFile && !this.isReST || virtualFile?.isSupportedByRuff(project) == true
--- src/main/kotlin/insyncwithfoo/ryecharm/RyeCharm.kt
+++ src/main/kotlin/insyncwithfoo/ryecharm/RyeCharm.kt
@@ -2,7 +2,8 @@ package insyncwithfoo.ryecharm
 
 
 @Suppress("unused", "MemberVisibilityCanBePrivate")
-internal object Plugin {
+internal object RyeCharm {
+    const val ID = "insyncwithfoo.ryecharm"
     const val NAME = "RyeCharm"
     const val REPOSITORY = "InSyncWithFoo/ryecharm"
     const val ISSUE_TRACKER = "https://github.com/$REPOSITORY/issues"
