7d30196ea9848307c1edb16e8d79ace1bf060676
InSyncWithFoo <insyncwithfoo@gmail.com>
2024-11-05 21:19:15 +0000

    Notify when import optimizer/formatter fails to run

--- src/main/kotlin/insyncwithfoo/ryecharm/ruff/formatting/RuffFormatter.kt
+++ src/main/kotlin/insyncwithfoo/ryecharm/ruff/formatting/RuffFormatter.kt
@@ -1,29 +1,91 @@
 package insyncwithfoo.ryecharm.ruff.formatting
 
-import com.intellij.execution.ExecutionException
 import com.intellij.execution.process.CapturingProcessAdapter
 import com.intellij.execution.process.OSProcessHandler
 import com.intellij.execution.process.ProcessEvent
+import com.intellij.execution.process.ProcessOutput
 import com.intellij.formatting.service.AsyncDocumentFormattingService
 import com.intellij.formatting.service.AsyncFormattingRequest
 import com.intellij.formatting.service.FormattingService.Feature
-import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.vfs.toNioPathOrNull
 import com.intellij.psi.PsiFile
 import com.intellij.sh.formatter.ShExternalFormatter
 import com.jetbrains.python.black.BlackFormattingService
 import insyncwithfoo.ryecharm.Command
+import insyncwithfoo.ryecharm.addOpenPluginIssueTrackerAction
+import insyncwithfoo.ryecharm.addSeeOutputActions
 import insyncwithfoo.ryecharm.configurations.ruff.RuffTimeouts
 import insyncwithfoo.ryecharm.configurations.ruff.ruffConfigurations
 import insyncwithfoo.ryecharm.editorFactory
 import insyncwithfoo.ryecharm.errorNotificationGroup
+import insyncwithfoo.ryecharm.information
+import insyncwithfoo.ryecharm.informationNotificationGroup
 import insyncwithfoo.ryecharm.isSupportedByRuff
 import insyncwithfoo.ryecharm.message
 import insyncwithfoo.ryecharm.ruff.OneBasedRange
 import insyncwithfoo.ryecharm.ruff.commands.ruff
 import insyncwithfoo.ryecharm.ruff.getOneBasedRange
+import insyncwithfoo.ryecharm.runThenNotify
+import insyncwithfoo.ryecharm.unableToRunCommand
 import java.time.Duration
+
+
+private val ProcessOutput.isProbablySyntaxErrorFailure: Boolean
+    get() = stderrLines.lastOrNull()?.contains("Failed to parse") == true
+
+
+private fun AsyncFormattingRequest.cannotFormatMultipleRanges() {
+    val title = message("notifications.cannotFormatMultipleRanges.title")
+    val body = message("notifications.cannotFormatMultipleRanges.body")
+    
+    onError(title, body)
+}
+
+
+private fun AsyncFormattingRequest.noChange() {
+    onTextReady(null)
+}
+
+
+private fun Project.fileContainsSyntaxError(output: ProcessOutput) {
+    val title = message("notifications.fileContainsSyntaxError.title")
+    val body = message("notifications.fileContainsSyntaxError.body")
+    
+    informationNotificationGroup.information(title, body).runThenNotify(this) {
+        addSeeOutputActions(output)
+    }
+}
+
+
+private fun Project.unknownFormattingError(output: ProcessOutput) {
+    val title = message("notifications.unknownFormattingError.title")
+    val body = message("notifications.unknownFormattingError.body")
+    
+    informationNotificationGroup.information(title, body).runThenNotify(this) {
+        addSeeOutputActions(output)
+        addOpenPluginIssueTrackerAction()
+    }
+}
+
+
+private fun AsyncFormattingRequest.errorHappened(output: ProcessOutput) {
+    noChange()
+    
+    val project = context.project
+    
+    when (output.isProbablySyntaxErrorFailure) {
+        true -> project.fileContainsSyntaxError(output)
+        else -> project.unknownFormattingError(output)
+    }
+}
+
+
+private fun String.getOneBasedRange(offsetRange: TextRange): OneBasedRange {
+    val document = editorFactory.createDocument(this)
+    return document.getOneBasedRange(offsetRange)
+}
 
 
 private fun OSProcessHandler.addProcessTerminatedListener(action: CapturingProcessAdapter.(ProcessEvent) -> Unit) {
@@ -36,8 +98,38 @@ private fun OSProcessHandler.addProcessTerminatedListener(action: CapturingProce
 }
 
 
+private class RuffFormattingTask(private val request: AsyncFormattingRequest, command: Command) :
+    RuffFormatter.FormattingTask() {
+    
+    private val handler = command.processHandler
+    
+    override fun isRunUnderProgress() = true
+    
+    override fun run() = handler.run {
+        addProcessTerminatedListener { event ->
+            val configurations = request.context.project.ruffConfigurations
+            
+            handleOutput(event, snoozeError = configurations.snoozeFormattingTaskError)
+        }
+        startNotify()
+    }
+    
+    private fun CapturingProcessAdapter.handleOutput(event: ProcessEvent, snoozeError: Boolean) = when {
+        event.exitCode == 0 -> request.onTextReady(output.stdout)
+        snoozeError -> request.noChange()
+        else -> request.errorHappened(output)
+    }
+    
+    override fun cancel(): Boolean {
+        handler.destroyProcess()
+        return true
+    }
+    
+}
+
+
 /**
- * Format a document on reformat (`Ctrl` + `L`).
+ * Format a document on reformat (`Ctrl` + `Alt` + `L`).
  * 
  * @see ShExternalFormatter
  * @see BlackFormattingService
@@ -101,63 +193,26 @@ internal class RuffFormatter : AsyncDocumentFormattingService() {
         
         val context = request.context
         val project = context.project
-        val ruff = project.ruff ?: return null
+        val ruff = project.ruff
+        
+        if (ruff == null) {
+            project.unableToRunCommand()
+            return null
+        }
         
         val text = request.documentText
         val path = context.virtualFile?.toNioPathOrNull()
-        val range = text.getOneBasedRange(ranges.single())
+        val range = ranges.singleOrNull()?.let { text.getOneBasedRange(it) }
         
-        val command = ruff.format(text, path, range)
+        val command = ruff.format(text, path, range, quiet = false)
         
-        return try {
-            RuffFormattingTask(request, command)
-        } catch (error: ExecutionException) {
-            LOGGER.error(error)
-            request.onError(message("notifications.error.title"), error.message!!)
-            return null
-        }
+        return RuffFormattingTask(request, command)
     }
     
-    private fun AsyncFormattingRequest.cannotFormatMultipleRanges() {
-        onError(
-            message("notifications.cannotFormatMultipleRanges.title"),
-            message("notifications.cannotFormatMultipleRanges.body")
-        )
-    }
-    
-    private fun String.getOneBasedRange(offsetRange: TextRange): OneBasedRange {
-        val document = editorFactory.createDocument(this)
-        return document.getOneBasedRange(offsetRange)
-    }
-    
-    private class RuffFormattingTask(private val request: AsyncFormattingRequest, command: Command) : FormattingTask {
-        
-        private val handler = command.processHandler
-        
-        override fun isRunUnderProgress() = true
-        
-        override fun run() = handler.run {
-            addProcessTerminatedListener { event ->
-                val configurations = request.context.project.ruffConfigurations
-                
-                when {
-                    event.exitCode == 0 -> request.onTextReady(output.stdout)
-                    configurations.snoozeFormattingTaskError -> request.onTextReady(null)
-                    else -> request.onError(message("notifications.error.title"), output.stderr)
-                }
-            }
-            startNotify()
-        }
-        
-        override fun cancel(): Boolean {
-            handler.destroyProcess()
-            return true
-        }
-        
-    }
-    
-    companion object {
-        private val LOGGER = logger<RuffFormatter>()
-    }
+    /**
+     * Expose [AsyncDocumentFormattingService.FormattingTask] so that
+     * [RuffFormattingTask] doesn't have to be defined within [RuffFormatter].
+     */
+    abstract class FormattingTask : AsyncDocumentFormattingService.FormattingTask
     
 }
