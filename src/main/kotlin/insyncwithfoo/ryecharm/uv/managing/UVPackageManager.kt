eedd354b01f43142d814f2d6f7316fbc68f30912
InSync <insyncwithfoo@gmail.com>
2024-12-16 07:15:52 +0700

    Fix compatibility problems with 2024.3.1 (#36, #37)

--- src/main/kotlin/insyncwithfoo/ryecharm/uv/managing/UVPackageManager.kt
+++ src/main/kotlin/insyncwithfoo/ryecharm/uv/managing/UVPackageManager.kt
@@ -1,22 +1,22 @@
 package insyncwithfoo.ryecharm.uv.managing
 
 import com.intellij.execution.ExecutionException
-import com.intellij.openapi.application.writeAction
+import com.intellij.execution.RunCanceledByUserException
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.Sdk
-import com.intellij.openapi.roots.OrderRootType
-import com.intellij.openapi.vfs.VfsUtil
 import com.jetbrains.python.packaging.bridge.PythonPackageManagementServiceBridge
 import com.jetbrains.python.packaging.common.PythonPackage
 import com.jetbrains.python.packaging.common.PythonPackageSpecification
-import com.jetbrains.python.packaging.management.PythonPackageManager
+import com.jetbrains.python.packaging.pip.PipBasedPackageManager
 import com.jetbrains.python.packaging.pip.PipRepositoryManager
-import com.jetbrains.python.sdk.PythonSdkUpdater
 import insyncwithfoo.ryecharm.Command
-import insyncwithfoo.ryecharm.completedAbnormally
+import insyncwithfoo.ryecharm.isSuccessful
 import insyncwithfoo.ryecharm.runInBackground
 import insyncwithfoo.ryecharm.uv.commands.UV
 import insyncwithfoo.ryecharm.uv.parsePipListOutput
+
+
+private typealias Stdout = String
 
 
 /**
@@ -29,63 +29,44 @@ import insyncwithfoo.ryecharm.uv.parsePipListOutput
  * @see UVReportedError
  */
 @Suppress("UnstableApiUsage")
-internal class UVPackageManager(private val uv: UV, project: Project, sdk: Sdk) : PythonPackageManager(project, sdk) {
-    
-    override var installedPackages: List<PythonPackage> = emptyList()
+internal class UVPackageManager(private val uv: UV, project: Project, sdk: Sdk) : PipBasedPackageManager(project, sdk) {
     
     override val repositoryManager by lazy { PipRepositoryManager(project, sdk) }
     
-    override suspend fun reloadPackages(): Result<List<PythonPackage>> {
+    override suspend fun reloadPackagesCommand(): Result<List<PythonPackage>> {
         val command = uv.pipList()
         val output = project.runInBackground(command)
         val packages = parsePipListOutput(output.stdout)
         
-        if (output.completedAbnormally || packages == null) {
-            return Result.failure(UVReportedError(output))
+        return when {
+            output.isCancelled -> Result.failure(RunCanceledByUserException())
+            !output.isSuccessful || packages == null -> Result.failure(UVReportedError(output))
+            else -> Result.success(packages).also { installedPackages = packages }
         }
-        
-        return Result.success(packages).also { installedPackages = packages }
-    }
-    
-    /**
-     * @see com.jetbrains.python.packaging.management.PythonPackageManager.refreshPaths
-     */
-    private suspend fun refreshPaths() = writeAction {
-        val (async, recursive, reloadChildren) = Triple(true, true, true)
-        
-        VfsUtil.markDirtyAndRefresh(
-            async, recursive, reloadChildren,
-            *sdk.rootProvider.getFiles(OrderRootType.CLASSES)
-        )
-        PythonSdkUpdater.scheduleUpdate(sdk, project)
-    }
-    
-    private suspend fun refreshAndReload() = run {
-        refreshPaths()
-        reloadPackages()
     }
     
     /**
      * @param options Often (always?) empty
      */
-    override suspend fun installPackage(
+    override suspend fun installPackageCommand(
         specification: PythonPackageSpecification,
         options: List<String>
-    ): Result<List<PythonPackage>> =
+    ) =
         uv.add(specification).runAndGetResult()
     
-    override suspend fun updatePackage(specification: PythonPackageSpecification) =
+    override suspend fun updatePackageCommand(specification: PythonPackageSpecification) =
         uv.upgrade(specification).runAndGetResult()
     
-    override suspend fun uninstallPackage(pkg: PythonPackage) =
+    override suspend fun uninstallPackageCommand(pkg: PythonPackage) =
         uv.remove(pkg.name).runAndGetResult()
     
-    private suspend fun Command.runAndGetResult(): Result<List<PythonPackage>> {
+    private suspend fun Command.runAndGetResult(): Result<Stdout> {
         val output = project.runInBackground(this)
         
         return when {
-            output.completedAbnormally -> Result.failure(UVReportedError(output))
-            else -> refreshAndReload()
+            output.isCancelled -> Result.failure(RunCanceledByUserException())
+            !output.isSuccessful -> Result.failure(UVReportedError(output))
+            else -> Result.success(output.stdout)
         }
     }
     
