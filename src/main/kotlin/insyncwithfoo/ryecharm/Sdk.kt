3f704ab6f74c57efe43b2ed23f4ca2856788d402
InSyncWithFoo <insyncwithfoo@gmail.com>
2025-01-29 00:57:43 +0000

    Remove project generator, package manager and SDK-related

--- src/main/kotlin/insyncwithfoo/ryecharm/Sdk.kt
+++ src/main/kotlin/insyncwithfoo/ryecharm/Sdk.kt
@@ -1,13 +1,8 @@
 package insyncwithfoo.ryecharm
 
 import com.intellij.openapi.projectRoots.Sdk
-import insyncwithfoo.ryecharm.uv.sdk.UVSDKAdditionalData
 import java.nio.file.Path
 
 
 internal val Sdk.path: Path?
     get() = homePath?.toPathOrNull()
-
-
-internal val Sdk.isUV: Boolean
-    get() = sdkAdditionalData is UVSDKAdditionalData
