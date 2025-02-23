299933922dfd08e2c7061ed0cc64dafa2c2370fa
InSyncWithFoo <insyncwithfoo@gmail.com>
2025-02-23 06:21:39 +0000

    Remove legacy workaround in `build.gradle.kts`

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,9 +49,6 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation(libs.junit)
     
-    // https://youtrack.jetbrains.com/issue/IJPL-157292
-    testImplementation("org.opentest4j:opentest4j:1.3.0")
-    
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
         create(
