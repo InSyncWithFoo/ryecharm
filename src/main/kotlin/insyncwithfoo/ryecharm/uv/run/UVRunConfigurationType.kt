0b4d2fddc5d515600a6978631c1d6735e1d93343
InSyncWithFoo <insyncwithfoo@gmail.com>
2025-03-23 03:08:05 +0000

    Add document for run configuration classes

--- src/main/kotlin/insyncwithfoo/ryecharm/uv/run/UVRunConfigurationType.kt
+++ src/main/kotlin/insyncwithfoo/ryecharm/uv/run/UVRunConfigurationType.kt
@@ -7,18 +7,51 @@ import insyncwithfoo.ryecharm.message
 import insyncwithfoo.ryecharm.uv.run.custom.UVCustomTaskFactory
 
 
+// Upstream issue: https://youtrack.jetbrains.com/issue/PY-79031
+/**
+ * The umbrella type of all uv run configurations.
+ * 
+ * As with other types, this is displayed as an item
+ * within the "add" list of the run configuration dialog.
+ */
 internal class UVRunConfigurationType : ConfigurationType, DumbAware {
     
+    /**
+     * The name to be díplayed
+     * in the run configuration dialog.
+     */
     override fun getDisplayName() =
         message("runConfigurations.type.displayName")
     
+    /**
+     * A description to be displayed in some UI contexts.
+     * 
+     * Less visible than the display name.
+     */
     override fun getConfigurationTypeDescription() =
         message("runConfigurations.type.description")
     
+    /**
+     * The icon to be displayed
+     * in the run configuration dialog.
+     */
     override fun getIcon() = UVIcons.TINY_16
     
+    /**
+     * The ID to be used during (de)serialization
+     * of run configuration instances.
+     * 
+     * Must not change under any circumstances.
+     */
     override fun getId() = UVRunIDs.MAIN_TYPE
     
+    /**
+     * The factories by which the user can create
+     * run configuration instances of this type.
+     * 
+     * Each factory corresponds to a subpanel
+     * in the run configuration dialog.
+     */
     override fun getConfigurationFactories() =
         arrayOf(UVCustomTaskFactory(this))
     
--- src/main/kotlin/insyncwithfoo/ryecharm/uv/run/custom/UVCustomTaskFactory.kt
+++ src/main/kotlin/insyncwithfoo/ryecharm/uv/run/custom/UVCustomTaskFactory.kt
@@ -4,6 +4,7 @@ import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.openapi.project.Project
 import insyncwithfoo.ryecharm.UVIcons
+import insyncwithfoo.ryecharm.message
 import insyncwithfoo.ryecharm.uv.run.UVRunConfigurationType
 import insyncwithfoo.ryecharm.uv.run.UVRunIDs
 
@@ -12,6 +13,8 @@ internal class UVCustomTaskFactory(type: UVRunConfigurationType) : Configuration
     
     override fun getId() = UVRunIDs.CUSTOM_FACTORY
     
+    override fun getName() = message("runConfigurations.factory.customTask.name")
+    
     override fun getIcon() = UVIcons.TINY_16
     
     override fun isEditableInDumbMode() = true
@@ -19,6 +22,13 @@ internal class UVCustomTaskFactory(type: UVRunConfigurationType) : Configuration
     override fun getOptionsClass() =
         UVCustomTaskSettings::class.java
     
+    /**
+     * Create a new run configuration instance from [template].
+     * 
+     * [template] could be a run configuration saved during previous IDE sessions.
+     * The default implementation simply passes it back to the caller.
+     * It is unknown why this is the case.
+     */
     override fun createConfiguration(name: String?, template: RunConfiguration) =
         UVCustomTask(name.orEmpty(), template.project, factory = this)
     
