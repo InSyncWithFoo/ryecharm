115dfa56078fa046b006bae659b70e7d4cb99f26
InSync <insyncwithfoo@gmail.com>
2025-04-13 07:38:41 +0700

    Show rule documentation in tooltips (#61)

--- src/main/kotlin/insyncwithfoo/ryecharm/ruff/Tooltips.kt
+++ src/main/kotlin/insyncwithfoo/ryecharm/ruff/Tooltips.kt
@@ -1,11 +1,16 @@
 package insyncwithfoo.ryecharm.ruff
 
+import insyncwithfoo.ryecharm.DocumentationURI
 import insyncwithfoo.ryecharm.configurations.ruff.RuffConfigurations
 import insyncwithfoo.ryecharm.toHTML
 
 
 internal fun RuffConfigurations.getFormattedTooltip(message: String, rule: RuleCode?): String {
-    val formatted = tooltipFormat % Pair(message, rule)
+    val rulePossiblyWithLink = when (rule != null && renderTooltips) {
+        true -> "[$rule](${DocumentationURI.ruffRule(rule)})"
+        else -> rule
+    }
+    val formatted = tooltipFormat % Pair(message, rulePossiblyWithLink)
     
     return when (renderTooltips) {
         true -> formatted.toHTML()
