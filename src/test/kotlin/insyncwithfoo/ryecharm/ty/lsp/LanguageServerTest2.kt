65eaa14705e054e48e4ec9654676f38b058f4516
InSyncWithFoo <insyncwithfoo@gmail.com>
2025-07-21 14:17:11 +0000

    Again

---
+++ src/test/kotlin/insyncwithfoo/ryecharm/ty/lsp/LanguageServerTest2.kt
@@ -0,0 +1,25 @@
+package insyncwithfoo.ryecharm.ty.lsp
+
+import com.intellij.platform.lsp.tests.checkLspHighlighting
+import com.intellij.testFramework.TestDataPath
+import com.intellij.testFramework.builders.ModuleFixtureBuilder
+import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase
+import com.intellij.testFramework.fixtures.ModuleFixture
+import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl
+
+
+@TestDataPath($$"$CONTENT_ROOT/testData")
+internal class LanguageServerTest2 : CodeInsightFixtureTestCase<ModuleFixtureBuilder<ModuleFixture>>() {
+    
+    override fun setUp() {
+        super.setUp()
+        (myFixture as CodeInsightTestFixtureImpl).canChangeDocumentDuringHighlighting(true)
+        myFixture.testDataPath = "testData/ty/lsp/LanguageServerTest"
+    }
+    
+    fun `test invalid assignment`() {
+        myFixture.configureByFile("invalid-assignment.py")
+        myFixture.checkLspHighlighting()
+    }
+    
+}
